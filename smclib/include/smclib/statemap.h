b57814f4dab8ac73359e688345a312f3d1c68359
Mikael Arguedas <mikael@osrfoundation.org>
2017-07-07 16:48:31 -0700

    remove trailing whitespaces (#26)

--- smclib/include/smclib/statemap.h
+++ smclib/include/smclib/statemap.h
@@ -6,20 +6,20 @@
 // License Version 1.1 (the "License"); you may not use this file
 // except in compliance with the License. You may obtain a copy
 // of the License at http://www.mozilla.org/MPL/
-// 
+//
 // Software distributed under the License is distributed on an
 // "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
 // implied. See the License for the specific language governing
 // rights and limitations under the License.
-// 
+//
 // The Original Code is State Machine Compiler (SMC).
-// 
+//
 // The Initial Developer of the Original Code is Charles W. Rapp.
 // Portions created by Charles W. Rapp are
 // Copyright (C) 2000 - 2007. Charles W. Rapp.
 // All Rights Reserved.
 //
-// Contributor(s): 
+// Contributor(s):
 //
 // Namespace
 //	statemap
@@ -32,7 +32,7 @@
 // Notes
 //  The finite state machine needs to be initialized to the
 //  starting state of the FSM.  This must be done manually in
-//  the constructor of the derived class. 
+//  the constructor of the derived class.
 //
 // Author
 //	C. W. Rapp
