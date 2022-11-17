038933e0978c8e982cebe93f088c64e7922f8710
Audrow Nash <audrow@openrobotics.org>
2022-11-17 08:53:07 -0600

    Update maintainers to Geoffrey Biggs
    Signed-off-by: Audrow Nash <audrow@openrobotics.org>

--- bondpy/setup.py
+++ bondpy/setup.py
@@ -6,7 +6,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['bondpy'],
-    package_dir={'': 'python'}
+    package_dir={'': 'python'},
+    maintainer='Geoffrey Biggs',
+    maintainer_email='geoff@openrobotics.org',
 )
 
 setup(**d)
