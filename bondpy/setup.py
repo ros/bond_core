318119347c0b41bf0d5f51de0b10540390483240
antoine <grs.antoine@outlook.fr>
2023-09-20 01:59:13 +0200

    add 'data_files' argument for marker file

--- bondpy/setup.py
+++ bondpy/setup.py
@@ -9,6 +9,11 @@ d = generate_distutils_setup(
     package_dir={'': 'python'},
     maintainer='Geoffrey Biggs',
     maintainer_email='geoff@openrobotics.org',
+    data_files=[
+        ('share/' + package_name, ['package.xml']),
+        ('share/ament_index/resource_index/packages',
+        ['resource/' + package_name])
+    ]
 )
 
 setup(**d)
