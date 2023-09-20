0f26c9e7810913f64d16efc86f86ebb289adae92
antoine <grs.antoine@outlook.fr>
2023-09-20 02:01:59 +0200

    adapt to current source code architecture

--- bondpy/setup.py
+++ bondpy/setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['bondpy'],
-    package_dir={'': 'python'},
+    package_dir={'': ''},
     maintainer='Geoffrey Biggs',
     maintainer_email='geoff@openrobotics.org',
     data_files=[
