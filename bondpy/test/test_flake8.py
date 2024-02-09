bf428c70daab9aab0df0bc1aaf13e04c123bba67
Chris Lalancette <clalancette@gmail.com>
2024-02-09 15:42:40 +0000

    Stop excluding BondSM_sm from flake8.
    This seems to be triggering some kind of bug in flake8
    itself.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- bondpy/test/test_flake8.py
+++ bondpy/test/test_flake8.py
@@ -19,7 +19,7 @@ import pytest
 @pytest.mark.flake8
 @pytest.mark.linter
 def test_flake8():
-    rc, errors = main_with_errors(argv=['--exclude=bondpy/BondSM_sm.py'])
+    rc, errors = main_with_errors(argv=[])
     assert rc == 0, \
         'Found %d code style errors / warnings:\n' % len(errors) + \
         '\n'.join(errors)
